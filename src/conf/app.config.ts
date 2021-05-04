f3221799e203dabb7d874d29e615ee768cea52e7
Khanatoshs <leo.hs334@gmail.com>
2021-05-04 11:54:28 -0500

    Ya no subiendo app.config

--- src/conf/app.config.ts
+++
@@ -1,9 +0,0 @@
-export interface ApplicationConfig {
-    accessKey: string;
-    secretKey:string;
-  }
-  
-  export const CONFIG: ApplicationConfig = {
-    accessKey: 'ACCESS_KEY_HERE',
-    secretKey: 'SECRET_KEY_HERE'
-  };
