423787a2d3d6aeda682b5987be64304c03f00d45
Khanatoshs <leo.hs334@gmail.com>
2021-05-04 11:58:58 -0500

    Agregado config file

---
+++ src/conf/app.config.ts
@@ -0,0 +1,9 @@
+export interface ApplicationConfig {
+    accessKey: string;
+    secretKey:string;
+  }
+  
+  export const CONFIG: ApplicationConfig = {
+    accessKey: 'ACCESS_KEY_HERE',
+    secretKey: 'SECRET_KEY_HERE'
+  };
